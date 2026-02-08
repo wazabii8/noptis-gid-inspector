e292ba6903f0f937b41b6842829c0b063b8b2a0f
Daniel Ronkainen <daniel.ronkainen@wazabii.se>
2026-02-08 19:26:31 +0100

    build: add actions for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base: "/noptis-gid-inspector/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
